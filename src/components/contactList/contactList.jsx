d457e050e1e3eba7c03edb894e861b98d795b54f
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2023-10-09 20:50:12 +0300

    fixed number

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -5,14 +5,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectorFiltred } from 'redux/selectors';
 export const ContactList = () => {
   const contactsList = useSelector(selectorFiltred);
-
+  console.log(contactsList);
   const dispatch = useDispatch();
   return (
     <div>
       {contactsList.map(item => {
         return (
           <Wrapper key={item.id}>
-            {item.name}: {item.number}
+            {item.name}: {item.phone}
             <StyledBtn onClick={() => dispatch(deleteContacts(item.id))}>
               Delete
             </StyledBtn>
